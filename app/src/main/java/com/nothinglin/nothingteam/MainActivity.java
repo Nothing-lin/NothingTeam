1438ac173deecf4dc6b1eab4785a7c63e86482b5
nothing-lin <1353695200@qq.com>
2022-08-09 22:21:46 +0800

    引入XUI界面框架，实现简单的对话框简易样式

--- app/src/main/java/com/nothinglin/nothingteam/MainActivity.java
+++ app/src/main/java/com/nothinglin/nothingteam/MainActivity.java
@@ -3,12 +3,31 @@ package com.nothinglin.nothingteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.xuexiang.xui.XUI;
+import com.xuexiang.xui.widget.dialog.materialdialog.MaterialDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //注册XUI主题
+        XUI.initTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //测试简单的对话框
+        findViewById(R.id.btn_test).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        new MaterialDialog.Builder(this)//getContext()==this
+                .iconRes(R.drawable.icon_tip)
+                .title(R.string.tip_infos)
+                .content(R.string.content_simple_confirm_dialog)
+                .positiveText(R.string.lab_submit)
+                .show();
     }
 }
