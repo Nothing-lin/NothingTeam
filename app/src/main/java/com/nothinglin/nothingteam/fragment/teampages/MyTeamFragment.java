76b7db537f594f4e2329a6d525fc3ad5e8d4ddd5
nothing-lin <1353695200@qq.com>
2022-08-14 16:35:10 +0800

    初始化团队交流页面的UI布局

---
+++ app/src/main/java/com/nothinglin/nothingteam/fragment/teampages/MyTeamFragment.java
@@ -0,0 +1,22 @@
+package com.nothinglin.nothingteam.fragment.teampages;
+
+import com.nothinglin.nothingteam.R;
+import com.nothinglin.nothingteam.base.BaseFragment;
+import com.xuexiang.xui.widget.actionbar.TitleBar;
+
+public class MyTeamFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_team_create;
+    }
+
+    @Override
+    protected TitleBar initTitle() {
+        return null;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
