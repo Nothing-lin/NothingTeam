44ce4cab48dc0297216abbf5cff9170886fd47db
nothing-lin <1353695200@qq.com>
2022-08-22 22:39:45 +0800

    实现欢迎界面 -- 退出账号

---
+++ app/src/main/java/com/nothinglin/nothingteam/activity/WelcomeActivity.java
@@ -0,0 +1,53 @@
+package com.nothinglin.nothingteam.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nothinglin.nothingteam.R;
+
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.android.api.model.UserInfo;
+
+/**
+ * app启动页面
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //启动界面延迟2秒
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initAfterWelcome();
+            }
+        },2000);
+
+
+
+
+    }
+
+    private void initAfterWelcome() {
+        //判断用户是否已经登录过了，如果登陆过了就跳转到main界面，如果没有登录记录的话就跳转回登录界面
+        //注意这个userinfo是极光的bean，不是我们自己定义的那个
+        UserInfo userInfo = JMessageClient.getMyInfo();
+
+        if (userInfo == null){
+            //如果极光服务器没有获取到登录记录的话就重新登录
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+        }else {
+            //如果已经登录过了就直接回到主界面
+            startActivity(new Intent(this,MainActivity.class));
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/nothinglin/nothingteam/fragment/MeFragment.java
+++ app/src/main/java/com/nothinglin/nothingteam/fragment/MeFragment.java
@@ -1,5 +1,6 @@
 package com.nothinglin.nothingteam.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.nothinglin.nothingteam.R;
+import com.nothinglin.nothingteam.activity.LoginActivity;
+import com.nothinglin.nothingteam.base.BaseFragment;
+import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xui.widget.actionbar.TitleBar;
+import com.xuexiang.xui.widget.textview.supertextview.SuperTextView;
 
-public class MeFragment extends Fragment {
+import butterknife.BindView;
+import cn.jpush.im.android.api.JMessageClient;
+@Page
+public class MeFragment extends BaseFragment {
 
-    @Nullable
+    @BindView(R.id.loginout)
+    SuperTextView mloginout;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_me,null);
-
-
-        return layout;
+    protected int getLayoutId() {
+        return R.layout.fragment_me;
     }
+
+    //初始化标题栏
+    @Override
+    protected TitleBar initTitle() {
+        //setImmersive是状态栏的设置，因为一开始已经取消了状态栏的样式了
+        super.initTitle().setLeftVisible(false).setImmersive(true);
+        return null;
+    }
+
+    @Override
+    protected void initViews() {
+
+        mloginout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JMessageClient.logout();
+                Intent intent = new Intent();
+                intent.setClass(getContext(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+    }
+
 }
