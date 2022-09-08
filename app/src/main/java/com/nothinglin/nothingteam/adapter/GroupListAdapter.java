5995d5361d8dfb56f8dfe339682be625ed05e142
nothing-lin <1353695200@qq.com>
2022-09-08 20:35:27 +0800

    群名片列表

---
+++ app/src/main/java/com/nothinglin/nothingteam/adapter/GroupListAdapter.java
@@ -0,0 +1,138 @@
+package com.nothinglin.nothingteam.adapter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.nothinglin.nothingteam.R;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import cn.jpush.im.android.api.callback.GetAvatarBitmapCallback;
+import cn.jpush.im.android.api.model.GroupInfo;
+import cn.jpush.im.android.api.model.UserInfo;
+
+/**
+ * Created by ${chenyn} on 2017/4/26.
+ */
+
+public class GroupListAdapter extends BaseAdapter {
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private String groupName;
+    private Map<Long, String> mGroupName = new HashMap<>();
+    private List<GroupInfo> mGroupInfo;
+
+    public GroupListAdapter(Context context, List<GroupInfo> groupInfo) {
+        this.mContext = context;
+        this.mInflater = LayoutInflater.from(context);
+        this.mGroupInfo = groupInfo;
+    }
+
+    @Override
+    public int getCount() {
+        return mGroupInfo.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mGroupInfo.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private String getGroupName(List<UserInfo> groupMembers, StringBuilder builder) {
+        for (UserInfo info : groupMembers) {
+            String noteName = info.getDisplayName();
+            builder.append(noteName);
+            builder.append(",");
+        }
+
+        return builder.substring(0, builder.lastIndexOf(","));
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.fragment_team_group_list_item, parent, false);
+            holder.itemLl = (LinearLayout) convertView.findViewById(R.id.group_ll);
+            holder.avatar = (ImageView) convertView.findViewById(R.id.group_iv);
+            holder.mGroupName = (TextView) convertView.findViewById(R.id.group_name);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        final GroupInfo groupInfo = mGroupInfo.get(position);
+        if (TextUtils.isEmpty(groupInfo.getGroupName())) {
+            //Conversation groupConversation = JMessageClient.getGroupConversation(groupId);
+            //群组名是null的话,手动拿出5个名字拼接
+            List<UserInfo> groupMembers = groupInfo.getGroupMembers();
+
+            StringBuilder builder = new StringBuilder();
+            if (groupMembers.size() <= 5) {
+                groupName = getGroupName(groupMembers, builder);
+            } else {
+                List<UserInfo> newGroupMember = groupMembers.subList(0, 5);
+                groupName = getGroupName(newGroupMember, builder);
+            }
+        } else {
+            groupName = groupInfo.getGroupName();
+        }
+
+        mGroupName.put(groupInfo.getGroupID(), groupName);
+        holder.mGroupName.setText(groupName);
+
+        groupInfo.getAvatarBitmap(new GetAvatarBitmapCallback() {
+            @Override
+            public void gotResult(int i, String s, Bitmap bitmap) {
+                if (i == 0) {
+                    holder.avatar.setImageBitmap(bitmap);
+                } else {
+                    holder.avatar.setImageResource(R.drawable.ic_cat);
+                }
+            }
+        });
+
+
+//        holder.itemLl.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Conversation groupConversation = JMessageClient.getGroupConversation(groupInfo.getGroupID());
+//                if (groupConversation == null) {
+//                    groupConversation = Conversation.createGroupConversation(groupInfo.getGroupID());
+//                    EventBus.getDefault().post(new Event.Builder()
+//                            .setType(EventType.createConversation)
+//                            .setConversation(groupConversation)
+//                            .build());
+//                }
+//
+//                Intent intent = new Intent(mContext, ChatActivity.class);
+//                intent.putExtra(JGApplication.CONV_TITLE, mGroupName.get(groupInfo.getGroupID()));
+//                intent.putExtra(JGApplication.GROUP_ID, groupInfo.getGroupID());
+//                mContext.startActivity(intent);
+//
+//            }
+//        });
+
+        return convertView;
+    }
+
+
+    private static class ViewHolder {
+        LinearLayout itemLl;
+        TextView mGroupName;
+        ImageView avatar;
+    }
+}
--- app/src/main/java/com/nothinglin/nothingteam/fragment/teampages/MyTeamFragment.java
+++ app/src/main/java/com/nothinglin/nothingteam/fragment/teampages/MyTeamFragment.java
@@ -1,13 +1,37 @@
 package com.nothinglin.nothingteam.fragment.teampages;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.nothinglin.nothingteam.R;
+import com.nothinglin.nothingteam.adapter.GroupListAdapter;
 import com.nothinglin.nothingteam.base.BaseFragment;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.android.api.callback.GetGroupIDListCallback;
+import cn.jpush.im.android.api.callback.GetGroupInfoCallback;
+import cn.jpush.im.android.api.model.GroupInfo;
+
 public class MyTeamFragment extends BaseFragment {
+
+
+    private GroupListAdapter mGroupListAdapter;
+    private Context mContext;
+
+    @BindView(R.id.group_list)
+    ListView mGroupList;
+
+
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_team_create;
+        return R.layout.fragment_team_group_list;
     }
 
     @Override
@@ -18,5 +42,38 @@ public class MyTeamFragment extends BaseFragment {
     @Override
     protected void initViews() {
 
+        mContext = getContext();
+
+        final List<GroupInfo> infoList = new ArrayList<>();
+        JMessageClient.getGroupIDList(new GetGroupIDListCallback() {
+            @Override
+            public void gotResult(int responseCode, String responseMessage, final List<Long> groupIDList) {
+                if (responseCode == 0) {
+                    final int[] groupSize = {groupIDList.size()};
+                    if (groupIDList.size() > 0) {
+                        for (Long id : groupIDList) {
+                            JMessageClient.getGroupInfo(id, new GetGroupInfoCallback() {
+                                @Override
+                                public void gotResult(int i, String s, GroupInfo groupInfo) {
+                                    if (i == 0) {
+                                        groupSize[0] = groupSize[0] - 1;
+                                        infoList.add(groupInfo);
+                                        if (groupSize[0] == 0) {
+                                            mGroupListAdapter = new GroupListAdapter(mContext, infoList);
+                                            mGroupList.setAdapter(mGroupListAdapter);
+                                        }
+
+                                    }
+                                }
+                            });
+                        }
+                    } else {
+                        Toast.makeText(getContext(), "您还没有群组", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getContext(), "出错："+responseMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
